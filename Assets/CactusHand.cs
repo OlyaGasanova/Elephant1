2daad3b36b8e0ad2f39cc835e21cfa819c05c7e7
GasanovaOlga <st040199@student.spbu.ru>
2017-08-17 21:01:47 +0300

    123

---
+++ Assets/CactusHand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusHand : MonoBehaviour {
+
+    // Use this for initialization
+   // public Component connectbody;
+
+    void Start()
+    {
+
+        Mesh holderMesh = new Mesh();
+        ObjImporter newMesh = new ObjImporter();
+        holderMesh = newMesh.ImportFile("C:/dev/CGAL-4.10/build/examples/Surface_mesh_segmentation/data/2_part_2.obj");
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = holderMesh;
+
+        GetComponent<MeshCollider>().sharedMesh = null;
+        GetComponent<MeshCollider>().sharedMesh = holderMesh;
+
+        GameObject cactusHand1 = GameObject.Find("CactusHand1");
+        cactusHand1.AddComponent<CharacterJoint>();
+        CharacterJoint connectbody = cactusHand1.GetComponent<CharacterJoint>();
+        Rigidbody rb = GameObject.Find("Cactus").GetComponent<Rigidbody>();
+
+        connectbody.connectedBody = rb;
+
+        // cactus1 = GameObject.Find("Cactus");
+        // Debug.Log("Succes");
+        // Debug.Log(cactus1.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/LoadModel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+
+public class LoadModel : MonoBehaviour
+{
+   
+    void Start()
+    {
+
+        Debug.Log("LoadModel");
+
+        string path = Directory.GetCurrentDirectory() + "\\Assets";
+
+        string[] dirs = Directory.GetFiles(path, "*.obj");
+        foreach (string dir in dirs)
+        {
+           // Debug.Log(dir);
+
+
+            GameObject CactusPart = new GameObject("CactusPart");
+            CactusPart.AddComponent<Rigidbody>();
+            CactusPart.GetComponent<Rigidbody>().isKinematic = false;
+            CactusPart.GetComponent<Rigidbody>().useGravity = false;
+            CactusPart.transform.position = new Vector3(0, 0, 0);
+
+
+            Mesh holderMesh = new Mesh();
+            ObjImporter newMesh = new ObjImporter();
+            holderMesh = newMesh.ImportFile(dir);
+
+            MeshRenderer renderer = CactusPart.AddComponent<MeshRenderer>();
+            MeshFilter filter = CactusPart.AddComponent<MeshFilter>();
+            filter.mesh = holderMesh;
+
+            CactusPart.AddComponent<MeshCollider>().sharedMesh = null;
+            CactusPart.GetComponent<MeshCollider>().sharedMesh = holderMesh;
+            CactusPart.GetComponent<MeshCollider>().convex = true;
+        }
+
+
+
+      
+
+
+    }
+
+    void OnCollisionEnter(Collision collision )
+    {
+
+        Debug.Log("Ono rabotaet!!!");
+
+
+
+    }
+
+
+
+    void Update()
+    {
+        
+       
+    }
+
+
+}
