7e3054f7f2e6788e94a5e17d5a1809f7fab21537
GasanovaOlga <st040199@student.spbu.ru>
2017-08-31 07:29:27 +0300

    CactusPart

---
+++ Assets/CactusPart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusPart : MonoBehaviour
+{
+    private List<ContactPoint> _contacts = new List<ContactPoint>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.red);
+            _contacts.Add(contact);
+        }
+        Debug.Break();
+    }
+    void OnDrawGizmos()
+    {
+        if (_contacts.Count > 0)
+        {
+            foreach (var contactPoint in _contacts)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(contactPoint.point, 0.1f);
+            }
+        }
+    }
+}
